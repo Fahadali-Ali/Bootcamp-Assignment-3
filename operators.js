524055e31ffbf554dce2b01b2c612e369375bdfe
Fahadali-Ali <20101001-045@uskt.edu.pk>
2024-08-15 09:28:21 +0500

    first commit

---
+++ operators.js
@@ -0,0 +1,15 @@
+// Declare Variables
+let num1 = 10;
+let num2 = 5;
+
+// Perform Arithmetic Operations
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+// Display Results
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
